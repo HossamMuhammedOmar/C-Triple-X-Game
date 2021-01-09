0937e4e7a2450b63f62b25d74f49febd25be4c04
Your Name <hossamomar9876@gmail.com>
2021-01-09 18:15:59 +0200

    update project

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "Welcome TO My First C++ Program";
+    std::cout << std::endl;
+    std::cout << "My First Console Game With C++";
     return 0;
 }
